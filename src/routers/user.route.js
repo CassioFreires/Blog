e44801773f9a00266d4957ee076f2e3bde7d371e
Cassio Souza <cassio_souza@live.com>
2024-01-12 15:19:25 -0300

    modified swagger.json for the best improvements

--- src/routers/user.route.js
+++ src/routers/user.route.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import userController from '../controllers/user.controller.js'
 import {validId, validUser} from '../middlewares/global.middlewares.js';
+import { authMidlewares } from '../middlewares/auth.middlewares.js';
 
 
 
@@ -11,9 +12,9 @@ const usersRouters = express.Router();
 
 
 usersRouters.post('/', userController.create);
-usersRouters.get('/', userController.findAll);
-usersRouters.get('/:id', validId, validUser, userController.findById);
-usersRouters.patch('/:id', validId, validUser, userController.updateOne);
+usersRouters.get('/', authMidlewares, userController.findAll);
+usersRouters.get('/:id', authMidlewares, validId, validUser, userController.findById);
+usersRouters.patch('/:id', authMidlewares, validId, validUser, userController.updateOne);
 
 
 export default  usersRouters;
